1a7c8daaf1779c3a371a53da546b990993e1b3fd
Daniel Sampaio <168040583+DanNikk0@users.noreply.github.com>
2024-06-18 21:31:51 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,12 +1,10 @@
 def int_to_roman(numero):
    
-    # Validação da entrada
     if not isinstance(numero, int):
-        raise ValueError("A entrada deve ser um número inteiro.")
+        raise ValueError
     if numero < 1 or numero > 3999:
-        raise ValueError("Número fora do intervalo. Deve ser entre 1 e 3999.")
+        raise ValueError
     
-    # Lista de valores e símbolos romanos
     valores = [
         (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
         (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
@@ -28,6 +26,4 @@ def contar_em_romano(limite):
         romano = int_to_roman(numero)
         print(f"{numero}: {romano}")
 
-# Chama a função para contar de 1 a 3999 em números romanos
 contar_em_romano(3999)
-
